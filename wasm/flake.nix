87b93924226c2aa8a3c8ebdda0828269fb2a1e1a
santoshxshrestha <santoshxshrestha@gmail.com>
2026-02-12 15:47:15 +0545

    init again

---
+++ wasm/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A nix flake for wasm projects in rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs =
+    { nixpkgs, naersk, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      naerskLib = pkgs.callPackage naersk { };
+    in
+    {
+      packages.${system}.default = naerskLib.buildPackage {
+        src = ./.;
+        buildInputs = [ pkgs.openssl ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.rust-analyzer
+          pkgs.clippy
+          pkgs.cargo-watch
+          pkgs.rustfmt
+
+          pkgs.nodejs
+          pkgs.pnpm
+          pkgs.nodePackages.typescript
+          pkgs.nodePackages.eslint
+          pkgs.nodePackages.prettier
+          pkgs.nodePackages.typescript-language-server
+
+          pkgs.wasm-pack
+          pkgs.rocmPackages.llvm.lld
+        ];
+
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
+      formatter = [
+        pkgs.rustfmt
+        pkgs.prettier
+      ];
+
+    };
+}
